2716af3fd2a4a9db027bdf7a6125c44c8c54c654
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-08 22:16:44 +0000

    Update c270270051.lua

--- script/c270270051.lua
+++ script/c270270051.lua
@@ -14,10 +14,12 @@ function s.initial_effect(c)
 	--Anti Targetting
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
 	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTargetRange(LOCATION_MZONE)
 	e2:SetTarget(aux.TargetBoolFunction(s.sclawfilter))
+	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_SCARECLAW}
