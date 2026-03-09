ca5ab094206af0728260ae9f2d42ae51ecfa578a
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-09 14:38:20 +0000

    Update c270270101.lua

--- script/c270270101.lua
+++ script/c270270101.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--On Summon set future fusion card
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SET)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
