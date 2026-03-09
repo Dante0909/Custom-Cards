0197c18c929002b06fc5967bb0ec2e83a276f045
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-09 14:30:12 +0000

    Added Cyber Dragon Mirai

---
+++ script/c270270101.lua
@@ -0,0 +1,30 @@
+--Cyber Mirai Dragon
+--Scripted by Hawky
+local s,id=GetID()
+function s.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_MACHINE),2,2,s.lcheck)
+	--Change name
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
+	e1:SetValue(CARD_CYBER_DRAGON)
+	c:RegisterEffect(e1)
+	--On Summon set future fusion card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SET)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_FUTURE_FUSION}
+function s.setfilter(c)
+	return c:IsSetCard(SET_FUTURE_FUSION) and c:IsSpellTrap() and c:IsSSetable()
+end
