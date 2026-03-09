ce5e5b4947828881eb8b82528267595f48a5e0b0
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-09 14:37:18 +0000

    Update c270270101.lua

--- script/c270270101.lua
+++ script/c270270101.lua
@@ -28,3 +28,22 @@ s.listed_series={SET_FUTURE_FUSION}
 function s.setfilter(c)
 	return c:IsSetCard(SET_FUTURE_FUSION) and c:IsSpellTrap() and c:IsSSetable()
 end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+endfunction s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
