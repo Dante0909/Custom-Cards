697ae9b543e316d85b726eb56d0aba314f4af358
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-11 00:06:55 +0000

    added Visas Pravus

---
+++ script/c270270052.lua
@@ -0,0 +1,23 @@
+--Visas Pravus
+--Scripted by Hawky
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon procedure
+		c:EnableReviveLimit()
+	--"Visas Starfrost" + 3 "Scareclaw" Monsters
+
+	Fusion.AddProcMix(c,true,true,CARD_VISAS_STARFROST,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_SCARECLAW),3)
+	
+end
+s.listed_names={CARD_VISAS_STARFROST}
+function s.matfilter(c)
+
+end
+function s.contactfil(tp)
+	local loc=LOCATION_ONFIELD|LOCATION_GRAVE
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then loc=LOCATION_ONFIELD end
+	return Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,loc,0,nil)
+end
+function s.contactop(g)
+	Duel.Remove(g,POS_FACEUP,REASON_COST|REASON_MATERIAL)
+end
