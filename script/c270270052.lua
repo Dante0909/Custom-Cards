2e77525df8f8e01550a43b2e6e485e936e0bd7e2
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-13 17:37:37 +0000

    Update c270270052.lua

--- script/c270270052.lua
+++ script/c270270052.lua
@@ -47,7 +47,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetTargetRange(1,0)
-	e1:SetTargets.splimit)
+	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
