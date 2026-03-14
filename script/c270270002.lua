06edb0307fcacef64665be714ccae12097fe9169
Dante0909 <mathgl2001@gmail.com>
2026-03-13 22:57:22 -0400

    prince

--- script/c270270002.lua
+++ script/c270270002.lua
@@ -5,12 +5,20 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	c:EnableCounterPermit(COUNTER_SPELL,LOCATION_PZONE|LOCATION_MZONE)
 	Pendulum.AddProcedure(c,false)
-	Fusion.AddProcMixN(c,false,false,s.matfilter,5)
-	Fusion.AddContactProc(c, s.contactfil,s.contactop)
+	Fusion.AddProcMixN(c,false,false,s.matfilter,1)
+	Fusion.AddContactProc(c, s.contactfil,s.contactop,false,nil,1)
+	
+	--only special once through its own effect
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e0:SetCondition(s.regcon)
+	e0:SetOperation(s.regop)
+	c:RegisterEffect(e0)
 	
 	--destroy st on summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
@@ -22,13 +30,12 @@ function s.initial_effect(c)
 	
 	--cannot target
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(1)
-	--c:RegisterEffect(e2)
+	c:RegisterEffect(e2)
 	
 	--place in pendulum zone
 	local e3=Effect.CreateEffect(c)
@@ -38,6 +45,7 @@ function s.initial_effect(c)
 	e3:SetCondition(s.pencon)
 	e3:SetTarget(s.pentg)
 	e3:SetOperation(s.penop)
+	e3:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e3)
 	
 	--activation
@@ -46,35 +54,56 @@ function s.initial_effect(c)
 	e4:SetCode(EFFECT_CANNOT_INACTIVATE)
 	e4:SetRange(LOCATION_PZONE)
 	e4:SetValue(s.effectfilter)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_DISABLE)
 	c:RegisterEffect(e4)
+	
 	--effect
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD)
 	e5:SetCode(EFFECT_CANNOT_DISEFFECT)
 	e5:SetRange(LOCATION_PZONE)
 	e5:SetValue(s.effectfilter)
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_DISABLE)
 	c:RegisterEffect(e5)
 	
+	--cannot disable effect on field
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD)
+	e7:SetCode(EFFECT_CANNOT_DISABLE)
+	e5:SetRange(LOCATION_PZONE)
+	e7:SetTargetRange(0,LOCATION_ONFIELD)
+	e7:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e7:SetTarget(s.indtg)
+	Duel.RegisterEffect(e7,0)
 	
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD)
-	e6:SetCode(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_CANNOT_DISABLE)
-	e6:SetRange(LOCATION_PZONE)
-	e6:SetValue(s.test)
-	--e6:SetTarget(LOCATION_ONFIELD)
-	--e6:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e6:SetTarget(s.indtg)
-	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_DISABLE)
-	c:RegisterEffect(e6)
+	local e8=Effect.CreateEffect(c)
+	e8:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e8:SetType(EFFECT_TYPE_IGNITION)
+	e8:SetRange(LOCATION_PZONE)
+	e8:SetCountLimit(1,{id,2})
+	e8:SetTarget(s.searchtg)
+	e8:SetOperation(s.searchop)
+	c:RegisterEffect(e8)
 	
 end
+
 s.counter_place_list={COUNTER_SPELL}
 s.listed_series={SET_ENDYMION}
 
-function s.splimit(e,se,sp,st)
-	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+function s.regcon(e)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	e1:SetTarget(s.splimit)
+	Duel.RegisterEffect(e1,tp)
+end
+
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsCode(id) and sumtype&SUMMON_TYPE_SPECIAL+1==SUMMON_TYPE_SPECIAL+1
 end
 
 function s.matfilter(c)
@@ -90,7 +119,7 @@ function s.contactop(g)
 end
 
 function s.destroyfilter(c)
-	return c:IsSpellTrap()
+	return c:IsSpell()
 end
 
 function s.destroytg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -127,14 +156,36 @@ end
 function s.effectfilter(e,ct)
 	local p=e:GetHandler():GetControler()
 	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
-	return --p==tp and
-		te:IsSpellEffect()
+	return te:IsSpellEffect()
 end
 
 function s.indtg(e,c)
 	return c:IsSpell()
 end
 
-function s.test(e,ct)
-	return true
+function s.thfilter(c)
+	return c:IsSetCard(SET_ENDYMION) and c:IsMonster() and c:IsAbleToHand()
+end
+
+function s.searchtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter, tp, LOCATION_DECK, 0, 1, nil) end
+	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+end
+
+function s.searchop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) and tc:HasLevel() and c:IsFaceup() and c:IsRelateToEffect(e) then
+		local cg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCanAddCounter,COUNTER_SPELL,1),tp,LOCATION_ONFIELD,0,nil)
+		local ct=tc:GetLevel()
+		if ct>0 and #cg>0 then
+		Duel.BreakEffect()
+			while ct>0 and #cg>0 do
+				cg:Select(tp,1,1,nil):GetFirst():AddCounter(COUNTER_SPELL,1)
+				ct=ct-1
+				cg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCanAddCounter,COUNTER_SPELL,1),tp,LOCATION_ONFIELD,0,nil)
+			end
+		end
+	end
 end
