069a9ffbed7df88233563bd6d2842ccbc43021aa
Hawky15 <hawke_danny@hotmail.co.uk>
2026-03-14 23:53:38 +0000

    Update c270270052.lua

--- script/c270270052.lua
+++ script/c270270052.lua
@@ -69,6 +69,12 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 		e4:SetValue(ct*200)
 		c:RegisterEffect(e4)
 	end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local reset=RESET_SELF_TURN
+		if Duel.IsTurnPlayer(tp) then reset=RESET_OPPO_TURN end
+		c:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END|reset,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
+		end
 function s.tgfilter(c)
     return c:IsAbleToGrave() and c:IsSetCard(SET_SCARECLAW)
 	end
@@ -78,12 +84,6 @@ function s.tgfilter(c)
 	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
-	end 
-		local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		local reset=RESET_SELF_TURN
-		if Duel.IsTurnPlayer(tp) then reset=RESET_OPPO_TURN end
-		c:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END|reset,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
 end
 function s.counterfilter(c)
 	return (c:IsSetCard(SET_SCARECLAW) or c:IsCode(65815684)) or c:GetSummonLocation()~=LOCATION_EXTRA
